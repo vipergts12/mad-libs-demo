4b6d112eb47f5923fe3bacfe70168b44a6e5bf4b
Oscar Enriquez <homecare@Mac-mini.home>
2020-08-23 15:43:46 -0400

    initial mad lib game

---
+++ mad_lib_game.py
@@ -0,0 +1,7 @@
+noun = input("enter a noun ")
+adjective = input("enter an adjective ")
+name = input("enter a name ")
+
+print("There once was a " + noun)
+print("whos was " +adjective)
+print("his name was " +name)
